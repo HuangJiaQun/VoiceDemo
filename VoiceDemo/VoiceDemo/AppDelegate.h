671174badc7ae713156d793294fc85217cc6cd17
HuangJiaQun <1582027251@qq.com>
2019-12-27 10:55:46 +0800

    Revert "Revert "Third party voice usage""
    This reverts commit e8079543c4abd3631dfa89934e65f2eb7b38156b.

---
+++ VoiceDemo/VoiceDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VoiceDemo
+//
+//  Created by 黄嘉群 on 2019/12/18.
+//  Copyright © 2019 黄嘉群. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

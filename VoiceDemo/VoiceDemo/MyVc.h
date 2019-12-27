1bd3a6e7f3dde54ffa3097dc35fe7e6b222faf1f
HuangJiaQun <1582027251@qq.com>
2019-12-27 12:23:54 +0800

    增加了一个类

---
+++ VoiceDemo/VoiceDemo/MyVc.h
@@ -0,0 +1,17 @@
+//
+//  MyVc.h
+//  VoiceDemo
+//
+//  Created by 黄嘉群 on 2019/12/27.
+//  Copyright © 2019 黄嘉群. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyVc : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
